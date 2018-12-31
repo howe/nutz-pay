f956653acfd1ab229f2e2f473cc4a05e9975d195
Howe Chiang <howechiang@gmail.com>
2018-12-31 13:45:14 +0800

    项目初始化

--- src/main/java/org/nutz/pay/bean/biz/Dict.java
+++ src/main/java/org/nutz/pay/bean/biz/Dict.java
@@ -7,6 +7,8 @@ package org.nutz.pay.bean.biz;
  */
 public class Dict {
 
-    public static String UMS_WEBPAY_API_GATEWAY = "https://qr.chinaums.com/netpay-portal/webpay/pay.do";
-    public static String UMS_WEBPAY_API_DEV_GATEWAY = "https://qr-test2.chinaums.com/netpay-portal/webpay/pay.do";
+    public static String UMS_WEBPAY_API_GET_GATEWAY = "https://qr.chinaums.com/netpay-portal/webpay/pay.do";
+    public static String UMS_WEBPAY_API_GET_DEV_GATEWAY = "https://qr-test2.chinaums.com/netpay-portal/webpay/pay.do";
+    public static String UMS_WEBPAY_API_POST_GATEWAY = "https://qr.chinaums.com/netpay-route-server/api/";
+    public static String UMS_WEBPAY_API_POST_DEV_GATEWAY = "https://qr-test2.chinaums.com/netpay-route-server/api/";
 }
