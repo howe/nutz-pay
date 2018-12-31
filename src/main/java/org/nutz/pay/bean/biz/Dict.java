9b8fed16b9983a79137f9e60b868b7bbb453c921
Howe Chiang <howechiang@gmail.com>
2018-12-31 14:54:37 +0800

    add: msg数据字典

--- src/main/java/org/nutz/pay/bean/biz/Dict.java
+++ src/main/java/org/nutz/pay/bean/biz/Dict.java
@@ -11,4 +11,41 @@ public class Dict {
     public static String UMS_WEBPAY_API_GET_DEV_GATEWAY = "https://qr-test2.chinaums.com/netpay-portal/webpay/pay.do";
     public static String UMS_WEBPAY_API_POST_GATEWAY = "https://qr.chinaums.com/netpay-route-server/api/";
     public static String UMS_WEBPAY_API_POST_DEV_GATEWAY = "https://qr-test2.chinaums.com/netpay-route-server/api/";
+
+    /**
+     * 微信公众号支付
+     */
+    public static String MSGTYPE_WXPAY_JSPAY = "WXPay.jsPay";
+    /**
+     * 支付宝
+     */
+    public static String MSGTYPE_TRADE_JSPAY = "trade.jsPay";
+    /**
+     * 全民付
+     */
+    public static String MSGTYPE_QMF_JSPAY = "qmf.jspay";
+    /**
+     * 无卡
+     */
+    public static String MSGTYPE_QMF_WEBPAY = "qmf.webPay";
+    /**
+     * 银联云闪付
+     */
+    public static String MSGTYPE_ACP_JSPAY = "acp.jsPay";
+
+    /**
+     * 微信小程序支付
+     */
+    public static String MSGTYPE_WX_UNIFIEDORDER = "wx.unifiedOrder";
+
+    /**
+     * MD5加密
+     */
+    public static String SIGNTYPE_MD5 = "MD5";
+
+    /**
+     * SHA256加密
+     */
+    public static String SIGNTYPE_SHA256 = "SHA256";
+
 }
