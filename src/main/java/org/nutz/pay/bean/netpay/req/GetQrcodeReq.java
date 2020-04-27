2ca8bf02198568a14b87b9a26b46da4573229c31
Howe <howechiang@gmail.com>
2020-04-27 16:09:52 +0800

    update: webpayApi.create 新增 微信子商户appId

---
+++ src/main/java/org/nutz/pay/bean/netpay/req/GetQrcodeReq.java
@@ -0,0 +1,9 @@
+package org.nutz.pay.bean.netpay.req;
+
+/**
+ * Copyright 2018 Freshplay Co ltd
+ *
+ * @author howechiang
+ */
+public class GetQrcodeReq {
+}
---
+++ src/main/java/org/nutz/pay/bean/netpay/resp/GetQrcodeResp.java
@@ -0,0 +1,9 @@
+package org.nutz.pay.bean.netpay.resp;
+
+/**
+ * Copyright 2018 Freshplay Co ltd
+ *
+ * @author howechiang
+ */
+public class GetQrcodeResp {
+}
